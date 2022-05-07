e21227b8d420baccf8a9b0c5d19513712d18b1f9
ShaharShaki <shaharshaki217@gmail.com>
2022-05-07 12:42:28 +0300

    Add project files.

---
+++ Whats-App-Web-Server/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Whats_App_Web_Server.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
