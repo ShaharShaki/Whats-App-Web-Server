1958240b144e7c5ff1417c7d901ae246dee16f32
ereleventer <erel@optibus.com>
2022-05-14 10:54:45 +0300

    api project

---
+++ What-App-Web-API/Contacts.cs
@@ -0,0 +1,17 @@
+﻿namespace Whats_App_Web_Server.Models
+{
+    public class Contacts
+    {
+
+        public int Id { get; set; }
+
+        public string name { get; set; }
+
+        public string last_seen { get; set; }
+
+        public string pic { get; set; }
+
+        public List<Messages> messages { get; set; }
+
+    }
+}
